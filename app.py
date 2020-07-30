1530119bbf49dd3dc0e5fed03c40816b666be6f3
sarmijavier <hjsarmiento1999@gmail.com>
2020-07-29 20:07:08 -0500

    templates and configuration with flask

--- app.py
+++ app.py
@@ -1,11 +1,25 @@
 from flask import Flask, render_template
-app = Flask(__name__)
+from flask_bootstrap import Bootstrap
+
+app = Flask(__name__, template_folder='templates', static_folder='static')
+bootstrap = Bootstrap(app)
 
 @app.route('/')
 def hello_world():
-    return render_template('index.html')
+    return render_template('presentation.html')
 
 
+@app.route('/whoami')
+def whoami():
+    return render_template('whoami.html')
+
+@app.route('/portfolio')
+def portfolio():
+    return render_template('portfolio.html')
+
+@app.route('/blog')
+def blog():
+    return render_template('blog.html')
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=1)
