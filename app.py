a1fac2105139c2e62ac11690a71292b132af2436
sarmijavier <hjsarmiento1999@gmail.com>
2020-08-01 18:35:45 -0500

    website2.0

--- app.py
+++ app.py
@@ -1,8 +1,34 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash, redirect, request, url_for
 from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from wtforms.fields import StringField, PasswordField, SubmitField, TextAreaField
+from wtforms.validators import DataRequired
+from wtforms.fields.html5 import EmailField
+import pyrebase
+import os
 
 app = Flask(__name__, template_folder='templates', static_folder='static')
 bootstrap = Bootstrap(app)
+SECRET_KEY = os.urandom(32)
+app.config['SECRET_KEY']= SECRET_KEY
+
+firebaseConfig = {
+  "apiKey": "AIzaSyDwM0tnkHrJNwWU6ETFSQVzE9h4lrRqNxw",
+  "authDomain": "messageswebsite-4130e.firebaseapp.com",
+  "databaseURL": "https://messageswebsite-4130e.firebaseio.com",
+  "projectId": "messageswebsite-4130e",
+  "storageBucket": "messageswebsite-4130e.appspot.com",
+  "messagingSenderId": "572331620753",
+  "appId": "1:572331620753:web:addd03690fda0d335ec230"
+}
+
+firebase = pyrebase.initialize_app(firebaseConfig)
+
+class ContactForm(FlaskForm):
+    email = EmailField('Email', validators=[DataRequired()])
+    message = TextAreaField('Message', validators=[DataRequired()])
+    submit = SubmitField('Send')
+
 
 @app.errorhandler(404)
 def not_found(error):
@@ -10,21 +36,47 @@ def not_found(error):
 
 
 @app.route('/')
-def hello_world():
-    return render_template('presentation.html')
-
-
-@app.route('/whoami')
 def whoami():
-    return render_template('whoami.html')
+    contact_form = ContactForm()
+
+    return render_template('whoami.html', contact_form=contact_form)
+
 
 @app.route('/portfolio')
 def portfolio():
-    return render_template('portfolio.html')
+
+    contact_form = ContactForm()
+
+    return render_template('portfolio.html', contact_form=contact_form)
 
 @app.route('/blog')
 def blog():
-    return render_template('blog.html')
+    contact_form = ContactForm()
+
+    return render_template('blog.html', contact_form=contact_form)
+
+@app.route('/neverStopLearning')
+def never():
+    return render_template('neverStopLearning.html')
+
+
+@app.route('/contact', methods=['GET','POST'])
+def contact():
+
+    contact_form = ContactForm()
+
+    if contact_form.validate_on_submit():
+        email = contact_form.email.data
+        message =  contact_form.message.data
+        flash('Thank you, I\'ll contact you soon')
+        data = {
+            'email': email,
+            'message': message
+        }
+        db = firebase.database()
+        db.child('messages').push(data)
+        return redirect(url_for('whoami'))
+
 
 if __name__ == "__main__":
     app.run(debug=1)
