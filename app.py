f4a3802729a0dde9c606ac849b3173946cf6ea2a
sarmijavier <hjsarmiento1999@gmail.com>
2020-08-01 18:49:48 -0500

    505 error

--- app.py
+++ app.py
@@ -30,6 +30,10 @@ class ContactForm(FlaskForm):
     submit = SubmitField('Send')
 
 
+@app.errorhandler(500)
+def server_error(error):
+    return render_template('505.html', error=error)
+
 @app.errorhandler(404)
 def not_found(error):
     return render_template('404.html', error=error)
